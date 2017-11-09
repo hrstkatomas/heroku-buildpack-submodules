47b2841d2c7be425ca82370c05a7a478b9636d3e
Ing. Tomas Hrstka <tomas.hrstka@factorio.cz>
2017-11-09 09:43:21 +0100

    added log

--- bin/install_git_submodules.rb
+++ bin/install_git_submodules.rb
@@ -9,6 +9,7 @@ config.get_params.each do |param|
   next unless param.match(/^submodule/)
   c = config[param]
 
+  puts "---> parsing ENV branch: #{ENV['BUILDPACK_BRANCH']}"
   branch_flag = ENV['BUILDPACK_BRANCH'] ? "-b #{ENV['BUILDPACK_BRANCH']}" : c["branch"] ? "-b #{c['branch']}" : ""
   puts "---> Installing submodule #{c["path"]} #{branch_flag}"
   `git clone #{c["url"]} #{branch_flag} #{ENV['BUILD_DIR']}/#{c["path"]}`
