86e2a944e5f64f0d8d9fb44dc23e49d64978b9b1
Ing. Tomas Hrstka <tomas.hrstka@factorio.cz>
2017-12-14 10:23:59 +0100

    inspecting evironment varibales for buildpacks

--- bin/install_git_submodules.rb
+++ bin/install_git_submodules.rb
@@ -5,6 +5,10 @@ require 'parseconfig'
 
 config = ParseConfig.new("#{ENV['BUILD_DIR']}/.gitmodules")
 
+puts "---> Buildpack environment variables"
+puts ENV['BUILD_DIR'], ENV['CACHE_DIR'], ENV['ENV_DIR'],
+puts ENV
+
 config.get_params.each do |param|
   next unless param.match(/^submodule/)
   c = config[param]
