001f2f5185df0d30f155fe2597a92cf50a793358
Ing. Tomas Hrstka <tomas.hrstka@factorio.cz>
2017-12-15 09:22:30 +0100

    strategy change

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# THIS IS A COPY OF [heroku-buildpack-submodules by Damien Mathieu](https://github.com/dmathieu/heroku-buildpack-submodules) BUT FOR DEVELOP BRANCH OF SUBMODULE
+# THIS IS A COPY OF [heroku-buildpack-submodules by Damien Mathieu](https://github.com/dmathieu/heroku-buildpack-submodules) THAT ALWAYS DOWNLOADS MASTER BRANCH
--- bin/install_git_submodules.rb
+++ bin/install_git_submodules.rb
@@ -2,14 +2,13 @@ require 'rubygems'
 Gem.clear_paths
 
 require 'parseconfig'
-
 config = ParseConfig.new("#{ENV['BUILD_DIR']}/.gitmodules")
 
 config.get_params.each do |param|
   next unless param.match(/^submodule/)
   c = config[param]
 
-  branch_flag = "-b develop"
+  branch_flag = "master"
   puts "---> Installing submodule #{c["path"]} #{branch_flag}"
   `git clone #{c["url"]} #{branch_flag} #{ENV['BUILD_DIR']}/#{c["path"]}`
 
